fe4afd61a87ad98d738d09a63eb4d9d41639e0a5
Malfurius <henneberg.maximilian@googlemail.com>
2018-12-15 12:05:02 +0100

    ex6

--- hw06.ml
+++ hw06.ml
@@ -134,7 +134,7 @@ let crawl cmd tree =
     match cmd with
     | Left::xs -> recCrawl xs tree stack l@prev
     | Right::xs -> recCrawl xs tree stack r@prev
-    | New v::xs -> recCrawl xs v stack prev
+    | New v::xs -> recCrawl xs (v,Empty,Empty) stack prev
     | [] -> tree
   in
    let _,_,_, t = recCrawl cmds tree [] [] in t
