cfcf94cef6a9a5568b3ec3a4187c5cee4855b128
Malfurius <henneberg.maximilian@googlemail.com>
2018-12-15 12:29:15 +0100

    ex6

--- hw06.ml
+++ hw06.ml
@@ -128,6 +128,7 @@ let rec eval_expr expr =
 
 (*****************************************************************************)
 (* Assignment 6.8 [7 points] *)
+(*
 let crawl cmd tree = 
   let rec recCrawl cmd tree stack prev=
     let Node (n,l,r) = tree
@@ -140,6 +141,26 @@ let crawl cmd tree =
     | [] -> tree
    in
    recCrawl cmds tree [] []
+   *)
+
+   let crawl cmds tree =
+  let rec impl cmds stack tree =
+    match cmds, stack, tree with
+    | [], s, t -> false, [], s, t
+    | Up::cs, ss, t -> true, cs, ss, t
+    | Left::cs, ss, Node (x, l, r) ->
+      let u, nc, ns, nt = impl cs ss l in
+      if u then impl nc ns (Node (x, nt, r)) else u, nc, ns, Node (x, nt, r)
+    | Right::cs, ss, Node (x, l, r) ->
+      let u, nc, ns, nt = impl cs ss r in
+      if u then impl nc ns (Node (x, l, nt)) else u, nc, ns, Node (x, l, nt)
+    | New v::cs, ss, _ -> impl cs ss (Node (v, Empty, Empty))
+    | Delete::cs, ss, _ -> impl cs ss Empty
+    | Push::cs, ss, t -> impl cs (t::ss) t
+    | Pop::cs, s::ss, _ -> impl cs ss s
+    | _ -> failwith "unexpected"
+  in
+  let _,_,_, t = impl cmds [] tree in t
 
 
 (*****************************************************************************)
