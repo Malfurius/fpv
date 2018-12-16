8d7cabee315c84b68861724cc92a5aaf43988fa6
Malfurius <henneberg.maximilian@googlemail.com>
2018-12-16 13:32:42 +0100

    Ex08

--- hw08.ml
+++ hw08.ml
@@ -83,7 +83,7 @@ let top n t =
 let map f t = 
   let rec trav f t = 
   let LNode(v,l,r) = t in
-  LNode((f v), (trav f (l() ) ), (trav f (r() ) ) )
+  LNode((f v),( fun () -> (trav f (l() ) ) ),(fun () -> (trav f (r() ) ) ))
   in
   trav f t
 
