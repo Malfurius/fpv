9a92f927a4f71e0f12d13fca06b5122bfd79c5ce
Malfurius <henneberg.maximilian@googlemail.com>
2018-12-16 10:53:23 +0100

    Ex08

--- hw08.ml
+++ hw08.ml
@@ -23,7 +23,7 @@ let rec insert v f t = match t with
    then Node(n,(insert v f l),r)
    else Node(n,l,(insert v f r)))
 
-let string_of_tree = Printf.print_string "Hello!"; flush std_out;;
+let string_of_tree = print_string "Hello!"; flush std_out;;
 
 let inorder_list = todo
 
