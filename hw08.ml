58277fd61ff24e53854ba12671d843186b0f907b
Malfurius <henneberg.maximilian@googlemail.com>
2018-12-16 23:05:23 +0100

    Ex08

--- hw08.ml
+++ hw08.ml
@@ -144,12 +144,9 @@ let insert_ vs cmp t =
 let is_inorder_list_tailrec () =
   ignore(inorder_list Empty);
   (* TODO: Tutors will check *)
-  (*
   let l = List.init 10000 (fun x -> x) in
   let t = insert_ l compare Empty in
   try ignore(inorder_list t); true with Stack_overflow -> false
-  *)
-  true
 
 let check_layer_tree r t =
   let rec impl n r (LNode (x, fl, fr)) =
