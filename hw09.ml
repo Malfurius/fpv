cdf98d4341529344875a8355c18f768016f8a720
Malfurius <henneberg.maximilian@googlemail.com>
2018-12-17 12:27:03 +0100

    Ex09

--- hw09.ml
+++ hw09.ml
@@ -24,7 +24,7 @@ let read_notes =
     let nList = readChildren list line in read nList
     with End_of_file -> list
   in
-  Note (List.rev (read []))
+  notes (List.rev (read []))
 
 (* 9.3 - 2 *)
 let read_wishlist = todo
