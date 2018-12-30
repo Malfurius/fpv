d5256d51c8529c1baf3cddc08c68d9eef879dfee
Malfurius <henneberg.maximilian@googlemail.com>
2018-12-30 13:40:53 +0100

    Ex9

--- hw09.ml
+++ hw09.ml
@@ -101,8 +101,8 @@ let write_list fName list =
   let file = open_out fName in
   let rec printItem nlist = 
     match nlist with
-    | x::xs -> Printf.fprintf file "%s;" x; printItem xs
-    | x::[] -> Printf.fprintf file "%s" x
+    | x::xs -> Printf.fprintf file "%s;" x; debugString x ;printItem xs
+    | x::[] -> Printf.fprintf file "%s" x;debugString x
   in
   printItem list; close_out file
 
