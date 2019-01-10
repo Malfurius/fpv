6d09d21d9aacf6d956c5e89c484b4874c9ca54bf
Malfurius <henneberg.maximilian@googlemail.com>
2019-01-10 14:32:35 +0100

    Ex10

--- hw10.ml
+++ hw10.ml
@@ -73,12 +73,6 @@ end
       | x::xs -> if(List.mem x b)then (recUnion xs b (x::l))else(recUnion xs b l)
       | [] -> l
 
-  let rec listToString l f =
-    let module RING = f in
-    match l with
-    | x::xs -> RING.to_string x; listToString xs f
-    | [] -> ()
-
 module SetRing (F : FiniteRing) : Ring with type t = F.t list = struct
   type t = F.t list
   let zero = []
@@ -86,7 +80,7 @@ module SetRing (F : FiniteRing) : Ring with type t = F.t list = struct
   let add a b = (recUnion a b [])
   let mul a b = b
   let compare a b = -1
-  let to_string a = listToString a F
+  let to_string a = List.iter F.to_string a
 end
 
 (*****************************************************************************)
