5eeba3f67c28c456865fc72af8fa3c963d0133d5
Malfurius <henneberg.maximilian@googlemail.com>
2019-01-11 14:52:41 +0100

    Ex10

--- hw10.ml
+++ hw10.ml
@@ -157,10 +157,10 @@ module DenseMatrix (F : Ring) : Matrix with type t = (F.t list list) and type el
   type t = (F.t list list)
   type elem = F.t
   let to_string m = (List.iter (fun x -> (printRow x F.to_string);debugString "\n") m);"test"
-  let create n m = let res = buildMatrix n m [] F.zero in to_string res;List.rev res
-  let identity n = let res = buildIDMatrix n n [] F.one F.zero in to_string res;res
-  let from_rows l = let res = List.rev (buildRowMatrix l []) in to_string res;res
-  let set r c v m = let res = (List.mapi (fun i x -> if(i=r)then(replace c v x)else(x)) m) in to_string res;res
+  let create n m = let res = buildMatrix n m [] F.zero in List.rev res
+  let identity n = let res = buildIDMatrix n n [] F.one F.zero
+  let from_rows l = let res = List.rev (buildRowMatrix l [])
+  let set r c v m = (List.mapi (fun i x -> if(i=r)then(replace c v x)else(x)) m)
   let get r c m = find c (findRow r m 0)
   let transpose m = let res = recTranspose m in to_string res; res
   let add a b = let res =  List.mapi (fun i x -> (List.mapi (fun j y -> (F.add y (get i j b) )) x)) a in to_string res; res
