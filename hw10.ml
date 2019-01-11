bf874fda01da04ff512e62be50aaaf00ece75e7b
Malfurius <henneberg.maximilian@googlemail.com>
2019-01-11 12:07:01 +0100

    Ex10

--- hw10.ml
+++ hw10.ml
@@ -145,7 +145,7 @@ module DenseMatrix (F : Ring) : Matrix with type t = (F.t list list) and type el
   type elem = F.t
   let to_string m = (List.iter (fun x -> (printRow x F.to_string);debugString "\n") m);"test"
   let create n m = let res = buildMatrix n m [] F.zero in to_string res;res
-  let identity n = buildIDMatrix n n [] F.one F.zero
+  let identity n = let res = buildIDMatrix n n [] F.one F.zero in to_string res;res
   let from_rows l = buildRowMatrix l []
   let set r c v m = m
   let get r c m = F.one
