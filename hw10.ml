f25ef7f9ac40bee47f0034a9309c16b88669cd1b
Malfurius <henneberg.maximilian@googlemail.com>
2019-01-13 00:00:10 +0100

    Ex10

--- hw10.ml
+++ hw10.ml
@@ -226,7 +226,10 @@ module SparseMatrix (F:Ring) : Matrix with type t = (int*int*((int*int*F.t) list
   (*
   let transpose (n,m,mat) = List.map (fun r -> List.mapi (fun (x,y,v) -> (y,x,v)) r) mat *)
   let transpose m = m
+  (*
   let add (n,m,a) (r,c,b) = List.mapi (fun i r ->  (addRow i m r a b get F.add F.zero)) (create n m)
+  *)
+  let add a b = a
   let mul a b = a
   let to_string = ""
 end
