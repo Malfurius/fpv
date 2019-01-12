466f5bbb2aa33ac0ef79eda8e8b391fce1b748ce
Malfurius <henneberg.maximilian@googlemail.com>
2019-01-12 14:16:54 +0100

    Ex10

--- hw10.ml
+++ hw10.ml
@@ -171,7 +171,7 @@ module DenseMatrix (F : Ring) : Matrix with type t = (F.t list list) and type el
   let from_rows l = l
   let set r c v m = (List.mapi (fun i x -> if(i=r)then(replace c v x)else(x)) m)
   let get r c m = find c (findRow r m 0)
-  let transpose m =let res =  myTranspose m create set in to_string res; res
+  let transpose m = m
   let add a b = let res =  List.mapi (fun i x -> (List.mapi (fun j y -> (F.add y (get i j b) )) x)) a in to_string res; res
   let mul a b = let res = List.mapi (fun i x -> perRow x b F.zero get F.add F.mul) a in to_string res; res
 end
