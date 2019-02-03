703aa8fe1b7058c515debcd924fb33db3081d020
Malfurius <henneberg.maximilian@googlemail.com>
2019-02-03 14:21:27 +0100

    B13

--- hw13.ml
+++ hw13.ml
@@ -154,7 +154,7 @@ module Array = struct
   let make s v =
     let rec sublist s l nl = match (l,s) with
     | (x::xs,_) -> sublist (s-1) (x::nl) xs
-    | (x::xs,0) -> List.rev nl
+    | (x::xs,0) -> nl
     | ([],_) -> raise OutOfBounds
     in
     let c = new_channel () in
